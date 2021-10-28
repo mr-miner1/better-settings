f30af925092fb8a9333c5cd30472f05afdba37a0
mr-miner1 <83544549+mr-miner1@users.noreply.github.com>
2021-10-28 11:36:24 +0530

    fix some bugs with v1.4

--- components/QuickActions.jsx
+++ components/QuickActions.jsx
@@ -2,7 +2,7 @@
 const { React } = require("powercord/webpack");
 const {
   Tooltip,
-  Icons: { GitHub, Gear },
+  Icons: { GitHub, Bin, Sync, Person },
 } = require("powercord/components");
 
 const { shell } = require("electron");
@@ -15,41 +15,41 @@ module.exports = ({ url, pluginpath, server, id }) => {
   return (
     <div className="quickactions">
       {constructor()}
-      {url != "none" && settings.get("qa_github") == true ? (
+      {url != "none" && settings.get("qa_github", true) == true ? (
         <Tooltip text="GitHub">
           <a id="better-settings-github" href={url} target="_blank">
             <GitHub />
           </a>
         </Tooltip>
       ) : null}
-      {settings.get("qa_folder") == true ? (
+      {settings.get("qa_folder", true) == true ? (
         <Tooltip text="Open Folder">
           <button
             id="better-settings-open"
             onClick={() => shell.openPath(pluginpath)}
           >
-            <img src="https://svgur.com/i/bWn.svg"></img>
+            <Sync />
           </button>
         </Tooltip>
       ) : null}
-      {settings.get("qa_remount") == true ? (
+      {settings.get("qa_remount", false) == true ? (
         <Tooltip text="Reload Plugin">
           <button
             id="better-settings-remount"
             onClick={() => powercord.pluginManager.remount(id)}
           >
-            <img src="https://svgur.com/i/bYQ.svg"></img>
+            <Sync />
           </button>
         </Tooltip>
       ) : null}
       {server != "none" ? (
         <Tooltip text="Support Server">
           <a id="better-settings-server" href={server} target="_blank">
-            <img src="https://svgur.com/i/bVs.svg"></img>
+            <Person />
           </a>
         </Tooltip>
       ) : null}
-      {settings.get("qa_delete") == true ? (
+      {settings.get("qa_delete", true) == true ? (
         <Tooltip text="Double Click To Delete Plugin">
           <button
             id="better-settings-delete"
@@ -57,23 +57,21 @@ module.exports = ({ url, pluginpath, server, id }) => {
               fs.rmdir(pluginpath, { recursive: true }, (err) => {});
               m.target.src = "https://svgur.com/i/bXm.svg";
               console.error("plugin deleted");
-              m.target.style.transform = "scale(1.2)";
+              m.target.style.color = "#ED4245";
               setTimeout(() => {
-                m.target.src = "https://svgur.com/i/bXm.svg";
+                m.target.style.color = "#ED4245";
               }, 500);
             }}
             onClick={(m) => {
               document.getElementsByClassName("powercord-product");
-              m.target.src = "https://svgur.com/i/bXm.svg";
-              m.target.style.filter = "brightness(2)";
+              m.target.style.color = "pink";
               setTimeout(() => {
-                m.target.src = "https://svgur.com/i/bX6.svg";
-                m.target.style.filter = "";
+                m.target.style.color = "white";
               }, 500);
               console.warn("Double Click To Delete Plugin");
             }}
           >
-            <img src="https://svgur.com/i/bX6.svg"></img>
+            <Bin />
           </button>
         </Tooltip>
       ) : null}
